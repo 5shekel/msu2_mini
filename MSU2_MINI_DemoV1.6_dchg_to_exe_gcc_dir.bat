29ca08154ccec5177f724ce8f9beb0ccdfed3e51
dchg43 <75658553@qq.com>
2025-02-14 21:49:30 +0800

    增加目录方式打包脚本

---
+++ MSU2_MINI_DemoV1.6_dchg_to_exe_gcc_dir.bat
@@ -0,0 +1,27 @@
+@echo off
+cd /D "%~dp0"
+
+set mainfilename=MSU2_MINI_DemoV1.6_dchg
+rem mkdir %mainfilename%.dist 2>nul
+
+setlocal enabledelayedexpansion
+
+for /f "delims=" %%i in ('where pyinstaller') do (
+    set "pypath=%%~dpi"
+)
+
+rem --lto=yes
+rem --standalone
+rem --onefile
+rem --windows-console-mode=disable
+python -m nuitka --standalone --windows-console-mode=disable ^
+	--enable-plugin=tk-inter --remove-output ^
+	--include-data-files="%~dp0/resource/*"="resource/" ^
+	--include-data-files="%pypath%../Lib/site-packages/HardwareMonitor/lib/*.dll"="HardwareMonitor/lib/" ^
+	--windows-icon-from-ico=resource/icon.ico ^
+	--output-filename=MSU2_mini.exe ^
+	%mainfilename%.py
+
+endlocal
+
+pause
