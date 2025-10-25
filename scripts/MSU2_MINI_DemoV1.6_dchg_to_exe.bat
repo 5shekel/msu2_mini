a417bee32c9104e0a90c7dbc73de769b36d64815
yair-mv <yair@mantis-vision.com>
2025-10-25 13:37:53 +0300

    move scripts to folder

---
+++ scripts/MSU2_MINI_DemoV1.6_dchg_to_exe.bat
@@ -0,0 +1,29 @@
+@echo off
+cd /D "%~dp0"
+
+set "datenow=%date:~5,2%%date:~8,2%"
+set "outfilename=MSU2_MINI_MG(by pyinstaller)-%datenow%.exe"
+move /y "%outfilename%" "%outfilename%.bak" 2>nul 1>nul
+
+setlocal enabledelayedexpansion
+
+for /f "delims=" %%i in ('where pyinstaller') do (
+    set "pypath=%%~dpi"
+)
+set "dllpath=%pypath%..\Lib\site-packages\HardwareMonitor\lib\*.dll"
+
+:: -F one file
+:: -D one dir
+:: -w no console
+pyinstaller -F -w -y --clean --distpath=dist ^
+    --add-data "resource;resource" ^
+    --add-binary="%dllpath%;HardwareMonitor\lib" ^
+    --icon "resource/icon.ico" ^
+    -n "%outfilename%" MSU2_MINI_DemoV1.6_dchg.py
+
+endlocal
+
+move /Y "dist\%outfilename%" .
+rmdir /S /Q .\dist\ .\build\
+del /Q "%outfilename%.spec"
+pause
---
+++ scripts/MSU2_MINI_DemoV1.6_dchg_to_exe_gcc.bat
@@ -0,0 +1,32 @@
+@echo off
+cd /D "%~dp0"
+
+set "mainfilename=MSU2_MINI_DemoV1.6_dchg"
+rem mkdir %mainfilename%.dist 2>nul
+
+set "datenow=%date:~5,2%%date:~8,2%"
+set "outfilename=MSU2_MINI_MG(by nuitka)-%datenow%.exe"
+move /y "%outfilename%" "%outfilename%.bak" 2>nul 1>nul
+
+setlocal enabledelayedexpansion
+for /f "delims=" %%i in ('where pyinstaller') do (
+    set "pypath=%%~dpi"
+)
+
+rem --lto=yes
+rem --standalone
+rem --onefile
+rem --windows-console-mode=disable
+python -m nuitka --onefile --clean-cache=all --windows-console-mode=disable ^
+	--enable-plugin=tk-inter --remove-output ^
+	--include-data-files="%~dp0/resource/*"="resource/" ^
+	--include-data-files="%pypath%../Lib/site-packages/HardwareMonitor/lib/*.dll"="HardwareMonitor/lib/" ^
+	--windows-icon-from-ico=resource/icon.ico ^
+	--output-filename="%outfilename%" ^
+	%mainfilename%.py
+
+endlocal
+
+::move /Y MSU2_mini.exe "%outfilename%"
+
+pause
